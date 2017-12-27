2226768371d06a34667540cd7862b2034d556076
dinggz1982 <dgz888@163.com>
2017-12-27 17:23:49 +0800

    ok

---
+++ src/main/java/com/chengtech/ChengtechApplication.java
@@ -0,0 +1,12 @@
+package com.chengtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChengtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChengtechApplication.class, args);
+	}
+}
